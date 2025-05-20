cf3c766d8d180d0505f35f3a62c74ca3ff627090
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-21 01:11:35 +0200

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
-
+import { StatusBar } from "expo-status-bar";
 export default function RootLayout() {
-  return <Stack />;
+  return (<>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: true }} />
+    </Stack>
+    <StatusBar style="light" />
+  </>
+  );
 }
---
+++ components/AppBarComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Appbar } from "react-native-paper";
+
+interface AppBarComponentProps {
+    title: string;
+}
+const AppBarComponent: React.FC<AppBarComponentProps> = {{ title }} => {
+    return (<Appbar.Header>
+        <Appbar.Content title={title} />
+    </Appbar.Header>)
+}
+export default AppBarComponent
