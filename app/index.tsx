5b028fa1144f75062f35de0f20f71d45170a979a
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-02 01:02:48 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import isDarkMode from "@/components/CheckDarkMode";
 import CustomButton from "@/components/CustomButton";
-import { Alert, Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
-
+import { Alert, Dimensions, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+;
 export default function Index() {
   const styles = StyleSheet.create({
     bg: {
@@ -12,10 +13,11 @@ export default function Index() {
     topview: { flex: 1, justifyContent: "center", alignItems: "center" },
     bottomview: { flex: 1, width: "100%", justifyContent: "flex-end", gap: 10, padding: 10 },
     title: { flex: 1, fontSize: 50, fontWeight: "bold", color: isDarkMode() ? "#fff" : "#000" },
+    safeareaview: { flex: 1 }
   },
   )
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeareaview}>
       <View
         style={{
           flex: 1,
