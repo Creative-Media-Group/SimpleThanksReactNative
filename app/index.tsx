e683f567f858a5e3959681e91271f56f0131df63
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-02 01:59:19 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
-import isDarkMode from "@/components/CheckDarkMode";
 import CustomButton from "@/components/CustomButton";
 import { Alert, Dimensions, StyleSheet, Text, View } from "react-native";
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 ;
 export default function Index() {
-  const insets = useSafeAreaInsets();
   const styles = StyleSheet.create({
     bg: {
       flex: 1, resizeMode: "cover",
@@ -12,8 +10,8 @@ export default function Index() {
       height: Dimensions.get("window").height
     },
     topview: { flex: 1, justifyContent: "center", alignItems: "center" },
-    bottomview: { flex: 1, width: "100%", justifyContent: "flex-end", gap: 10, padding: 10 },
-    title: { flex: 1, fontSize: 50, fontWeight: "bold", color: isDarkMode() ? "#fff" : "#000" },
+    bottomview: { flex: 1, width: "100%", justifyContent: "flex-end", gap: 10, padding: 10, alignContent: "center" },
+    title: { flex: 1, fontSize: 50, fontWeight: "bold" }, //color: isDarkMode() ? "#fff" : "#000"
     safeareaview: {
       flex: 1,
       justifyContent: "center",
@@ -23,12 +21,7 @@ export default function Index() {
   )
   return (
     <SafeAreaView style={styles.safeareaview}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: isDarkMode() ? "#000" : "#fff",
-        }}
-      >
+      <View>
         <View style={styles.topview}>
           <Text style={[styles.title]}>Thanks ...</Text></View>
         <View style={styles.bottomview}>
